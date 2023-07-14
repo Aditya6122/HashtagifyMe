81df38c2985a45911821ffb943025d5e7ac7edce
Aditya6122 <kailas20bcs61@iiitkottayam.ac.in>
2023-07-14 13:28:58 +0530

    some changes

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from routes import app
+from flask import Flask, request, render_template
+from flask_cors import CORS
+from PIL import Image
+from HashtagGenerator.inference import get_inference
+
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+     return render_template("index.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+     file = request.files['image']
+     raw_image = Image.open(file)
+     hashtags = list(get_inference(raw_image))
+     print(hashtags)
+     return hashtags
 
 if __name__ == '__main__':
-    port=8000
-    app.run(port=port, debug=True)
+    app.run(debug=True)
