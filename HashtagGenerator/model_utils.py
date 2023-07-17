af934fe33937bcd650387dda428700b94b42e28d
Aditya6122 <kailas20bcs61@iiitkottayam.ac.in>
2023-07-18 00:36:53 +0530

    back to build pass

--- HashtagGenerator/model_utils.py
+++ HashtagGenerator/model_utils.py
@@ -2,11 +2,8 @@ from transformers import BlipForConditionalGeneration
 from transformers import AutoProcessor
 from transformers import AutoConfig
 import torch
-import gdown
-import os
-import warnings
 
-local_path = 'model/new_model.pth'
+local_path = 'model/best_model.pth'
 model_url = 'Salesforce/blip-image-captioning-base'
 
 def load_processor():
@@ -17,21 +14,14 @@ def load_model(from_local=True):
     config = AutoConfig.from_pretrained(model_url)
 
     if from_local:
-        model =  BlipForConditionalGeneration(config)
-        if not os.path.isfile(local_path):
-            print("No local model file found !!")
-            print("Initiate to download from drive")
-            try:
-                remote_model_path = 'https://drive.google.com/uc?id=1vxmwsSSUQ0MTjfQ2uSAc9J96ezuoh9aa'
-                gdown.download(url=remote_model_path ,output=local_path,quiet=False)
-            except:
-                warnings.warn('Some unknown exception occured while downloading the model file.')
-        
-        model.load_state_dict(torch.load(local_path))
-        return model
+        try:
+            model =  BlipForConditionalGeneration(config)
+            model.load_state_dict(torch.load(local_path))
+            return model
+        except:
+            print("Local file for model not found !!")
+            print("Downloading the original model.")
 
-    warnings.warn("You are about to download the original \"BlipForConditionalGeneration\"\n \
-        This behaviour is not expected unless you are training it from scratch for Hashtag generation")
     model =  BlipForConditionalGeneration.from_pretrained(model_url)
     return model
 
