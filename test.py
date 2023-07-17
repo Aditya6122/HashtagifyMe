fb947b9cf5f5178307f93fdcd3af373041eb14cc
Aditya6122 <kailas20bcs61@iiitkottayam.ac.in>
2023-07-17 23:42:49 +0530

    Remote model file
    Downloading remote model file when local not found

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from urllib.request import urlopen
 
-url = "http://localhost:5000/predict" 
+url = "http://app:5000/predict" 
 image_path = "demo.png" 
 
 time = 0
@@ -14,8 +14,7 @@ while(time<60 and flag):
         print("waiting..from",time+1,"seconds")
         time += 1 
 
-with open(image_path, "rb") as file:
-    files = {"image": file}
-    response = requests.post(url, files=files, timeout=60)
-
-print(response.text)
+# with open(image_path, "rb") as file:
+#     files = {"image": file}
+#     response = requests.post(url, files=files, timeout=60)
+# print(response.text)
