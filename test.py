53b642e8ed4428bd11880fe89db976266168570a
Aditya6122 <kailas20bcs61@iiitkottayam.ac.in>
2023-07-17 23:02:08 +0530

    Remote model file
    Downloading remote model file when local not found

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
 import requests
+from urllib.request import urlopen
+from urllib.error import *
 
 url = "http://localhost:5000/predict" 
 image_path = "demo.png" 
+
+try:
+    html = urlopen(url)
+     
+except HTTPError as e:
+    print("HTTP error", e)
+     
+except URLError as e:
+    print("Opps ! Page not found!", e)
 
 with open(image_path, "rb") as file:
     files = {"image": file}
     response = requests.post(url, files=files, timeout=60)
 
-print(response)
-print(response.text)
-print(response.content)
+# print(response)
+# print(response.text)
+# print(response.content)
