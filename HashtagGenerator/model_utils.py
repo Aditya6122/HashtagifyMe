207729b72786b1baf6977a482a2a90260c9c4a29
Aditya6122 <kailas20bcs61@iiitkottayam.ac.in>
2023-07-22 04:15:00 +0530

    workflow setup

--- HashtagGenerator/model_utils.py
+++ HashtagGenerator/model_utils.py
@@ -23,9 +23,8 @@ def load_model(device,from_local=True):
             print("No local model file found !!")
             print("Initiate to download from drive")
             try:
-                proxy = urllib3.ProxyManager("https://localhost:3128/")
                 remote_model_path = 'https://drive.google.com/uc?id=1vxmwsSSUQ0MTjfQ2uSAc9J96ezuoh9aa'
-                gdown.download(url=remote_model_path ,output=local_path,quiet=False,proxy=proxy)
+                gdown.download(url=remote_model_path ,output=local_path,quiet=False,use_cookies=True)
             except:
                 raise Exception('Some unknown exception occured while fetching the remote model file. \n \
                     Check if file is present on the remote location')
