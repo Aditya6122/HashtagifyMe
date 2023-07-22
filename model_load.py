e69d34e83954bbd29ac5bb4ea71962abede9c342
Aditya6122 <kailas20bcs61@iiitkottayam.ac.in>
2023-07-22 14:38:35 +0530

    workflow setup

--- model_load.py
+++ model_load.py
@@ -3,6 +3,10 @@ from transformers import AutoProcessor
 from transformers import AutoConfig
 
 model_url = 'Salesforce/blip-image-captioning-base'
+print("loading processor")
 processor = AutoProcessor.from_pretrained(model_url)
+print("loading config")
 config = AutoConfig.from_pretrained(model_url)
+print("loading model")
 model =  BlipForConditionalGeneration(config)
+print("success")
